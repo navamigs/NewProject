cc153de8a9351a7f1d7aadbf292ca44a0fe3a33a
gsnavami <navamigs17@gmail.com>
2019-10-14 15:11:33 +0530

    first commit

---
+++ src/main/java/myProject/NewApp.java
@@ -0,0 +1,13 @@
+package myProject;
+
+public class NewApp {
+	//public String Sample() {
+	//	return "apple";
+	
+	public static void main( String[] args)
+	{
+		System.out.println("HELLO WORLD");
+	
+	}
+
+}
---
+++ src/test/java/myProject/NewAppTest.java
@@ -0,0 +1,14 @@
+package myProject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewAppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
